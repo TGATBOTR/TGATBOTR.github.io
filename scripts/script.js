4d0c0a17a3c28b3fbc7c3bc3564831a2d3d87d9b
Archey Barrell <archey.barrell@outlook.com>
2022-03-24 14:00:31 +0000

    Bootstrap

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,28 @@
 const iconsFolder = "/resources/icons/";
-const allergyIconPaths = 
-[
-	"celery",
-	"cereals",
-	"crustaceans",
-	"eggs",
-	"fish",
-	"lupin",
-	"milk",
-	"molluscs",
-	"mustard",
-	"tree_nuts",
-	"peanuts",
-	"sesame",
-	"soya",
-	"sulphites",
-].map(path => iconsFolder + path + ".png");
+const allergyIconPaths =
+	[
+		"celery",
+		"cereals",
+		"crustaceans",
+		"eggs",
+		"fish",
+		"lupin",
+		"milk",
+		"molluscs",
+		"mustard",
+		"tree_nuts",
+		"peanuts",
+		"sesame",
+		"soya",
+		"sulphites",
+	].map(path => iconsFolder + path + ".png");
 
-function createTableFromParamters()
-{
+function createTableFromParamters() {
 	const params = new URLSearchParams(window.location.search);
 	let tableObj = undefined;
 
-	for (const [name, value] of params)
-	{
-		if (name == "data")
-		{
+	for (const [name, value] of params) {
+		if (name == "data") {
 			const b64String = base64UrlTobase64(value);
 			const jsonString = atob(b64String);
 			tableObj = JSON.parse(jsonString);
@@ -42,31 +39,26 @@ function createTableFromParamters()
 	const tbody = createTableBody(tableObj.tContent);
 	table.appendChild(tbody);
 
-	const bodys = document.getElementsByTagName("body");
-	if (bodys.length > 0)
-	{
-		bodys[0].appendChild(table);
-	}
+	const bodys = document.getElementById('table_dest');
+
+
+	bodys.appendChild(table);
 }
 
-function createTableHead(headers, xOffset)
-{
+function createTableHead(headers, xOffset) {
 	const thead = document.createElement("thead");
 	const tr = document.createElement("tr");
 	thead.appendChild(tr);
 
-	if (xOffset !== undefined)
-	{
-		for (let i = 0; i < xOffset; i++)
-		{
+	if (xOffset !== undefined) {
+		for (let i = 0; i < xOffset; i++) {
 			const th = document.createElement("th");
 			th.appendChild(document.createTextNode(""));
 			tr.appendChild(th);
 		}
 	}
 
-	for (const header of headers)
-	{
+	for (const header of headers) {
 		const th = document.createElement("th");
 		const text = document.createTextNode(header);
 
@@ -77,11 +69,9 @@ function createTableHead(headers, xOffset)
 	return thead;
 }
 
-function createTableBody(contents)
-{
+function createTableBody(contents) {
 	const tbody = document.createElement("tbody");
-	for (const row of contents)
-	{
+	for (const row of contents) {
 		const tr = createTableRowWithImage(row);
 		tbody.appendChild(tr);
 	}
@@ -89,21 +79,25 @@ function createTableBody(contents)
 	return tbody;
 }
 
-function createTableRowWithImage(items)
-{
+function createTableRowWithImage(items) {
 	const tr = document.createElement("tr");
 
-	if (items.length > 0)
-	{
+	if (items.length > 0) {
 		const img = document.createElement("img");
 		img.src = getImagePath(items[0]);
-		
+
 		tr.appendChild(img);
-	
-		for (let i = 1; i < items.length; i++)
-		{
+
+		for (let i = 1; i < items.length; i++) {
 			const td = document.createElement("td");
-			td.appendChild(document.createTextNode(items[i]));
+
+			let output = "";
+
+			if (items[i] != "null") {
+				output = items[i];
+			}
+
+			td.appendChild(document.createTextNode(output));
 			tr.appendChild(td);
 		}
 	}
@@ -111,27 +105,22 @@ function createTableRowWithImage(items)
 	return tr;
 }
 
-function getImagePath(allergyId)
-{
-	if (allergyId >= 0 && allergyId < allergyIconPaths.length)
-	{
+function getImagePath(allergyId) {
+	if (allergyId >= 0 && allergyId < allergyIconPaths.length) {
 		return allergyIconPaths[allergyId];
 	}
 
 	return "";
 }
 
-function base64UrlTobase64(bString)
-{ // See https://stackoverflow.com/questions/5234581/base64url-decoding-via-javascript
+function base64UrlTobase64(bString) { // See https://stackoverflow.com/questions/5234581/base64url-decoding-via-javascript
 	bString = bString
 		.replace(/-/g, '+')
 		.replace(/_/g, '/');
 
 	const pad = bString.length % 4;
-	if (pad)
-	{
-		if (pad === 1)
-		{
+	if (pad) {
+		if (pad === 1) {
 			throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
 		}
 
